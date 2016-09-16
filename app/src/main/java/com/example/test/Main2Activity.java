640e5455dd6295df44a4e423765569f6119e1bc2
DavudD83 <DavudD83@gmail.com>
2016-09-17 00:27:27 +0300

    Добавил комментарии ко всему коду, сделал структуру по блокам, улучшил читабельность

--- app/src/main/java/com/example/test/Main2Activity.java
+++ app/src/main/java/com/example/test/Main2Activity.java
@@ -1,12 +1,9 @@
 package com.example.test;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
-import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,163 +12,226 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
 
-    LinearLayout linearLayout;
+    /*
+    Объекты классов
+     */
 
-    Button room1;
+    /*********************************************************/
 
+    //объект класса intent для перехода или передачи данных на активность roomsActivity
     Intent intent;
+
+    //объект класса intent для перехода или передачи данных на активность MainActivity
     Intent intent1;
 
-
-
+    //параметры для кнопок
     LinearLayout.LayoutParams layoutParams;
 
+    /*********************************************************/
+
+    /*
+    Элементы на экране
+     */
+
+    /*********************************************************/
+
+    //область на экране для размещения элементов
+    LinearLayout linearLayout;
+
+    //кнопки комнат
+    Button room1;
+    Button room2;
+    Button room3;
+
+    /*********************************************************/
+
+     /*
+    Метод для переопределения кнопки "назад"
+     */
+
+    /*********************************************************/
+
     @Override
-    public void onBackPressed() {//переопределяемая функция для кнопки "назад"
-        moveTaskToBack(true);//мы запрещаем выходить кнопкой "назад" на главную активность
-    }//вместо этого наше приложение уходит в "фон"
+    public void onBackPressed() {
+        //мы запрещаем выходить кнопкой "назад" на главную активность
+        //вместо этого наше приложение уходит в "фон"
+        moveTaskToBack(true);
+    }
 
+    /*********************************************************/
 
+    /*
+    Метод вызываемый при создании активности Main2Activity
+     */
+
+    /*********************************************************/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        intent = new Intent(Main2Activity.this,roomsActivity.class);
-        intent1 = new Intent(Main2Activity.this,MainActivity.class);
+        //инициализация объектов класса intent
+        intent = new Intent(Main2Activity.this, roomsActivity.class);
+        intent1 = new Intent(Main2Activity.this, MainActivity.class);
 
-
-
+        //нахождение элементана экране по его ID
         linearLayout = (LinearLayout) findViewById(R.id.lMain);
 
+        //инициализация объекта класса LinearLayout.LayoutParams
+        //WRAP_CONTENT - размер кнопок будет в зависимости от текста внутри
         layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayoutCompat.LayoutParams.WRAP_CONTENT);
 
+        //отступ сверху
         layoutParams.topMargin = 50;
 
+        //расположение по центру
         layoutParams.gravity = Gravity.CENTER;
 
+        //инициализация объектов класса Button
         room1 = new Button(this);
+        room2 = new Button(this);
+        room3 = new Button(this);
 
+        //установка ID для кнопок
         room1.setId(R.id.Room1);
-
-        room1.setText("Kitchen");
-
-
-        Button room2 = new Button(this);
-
         room2.setId(R.id.Room2);
-
-        room2.setText("Bathroom");
-
-        Button room3 = new Button(this);
-
         room3.setId(R.id.Room3);
 
+        //установка текста для кнопок
+        room1.setText("Kitchen");
+        room2.setText("Bathroom");
         room3.setText("Bedroom");
 
+        //инициализация массива состоящего из 3 кнопок
         Button [] Rooms = new Button[3];
 
+        //заполнение массива объектами
         Rooms[0] = room1;
         Rooms[1] = room2;
         Rooms[2] = room3;
 
-        for(int i = 0;i < Rooms.length;++i)
-            Rooms[i].setOnClickListener(this);
+        //делаем кнопки кликабельными
+        for (Button Room : Rooms)
+            Room.setOnClickListener(this);
 
-
-        GenerateRoomButton(Rooms,layoutParams);
-
+        //вызываем метод отрисовки кнопок на экран
+        GenerateRoomButton(Rooms, layoutParams);
     }
 
+    /*********************************************************/
+
+    /*
+    Обработчик нажатий кнопок
+     */
+
+    /*********************************************************/
 
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
+        switch (v.getId()) {
 
-
+            //нажата кнопка "Kitchen"
             case R.id.Room1:
+                //помещаем в intent значение 1,
                 intent.putExtra("Room", 1);
+
+                //выводим сообщение на экран
                 Toast.makeText(this, "You have choosen the kitchen", Toast.LENGTH_SHORT).show();
+
+                //переходим на активность roomsActivity
                 startActivity(intent);
                 break;
 
-
-
+            //нажата кнопка "Bathroom"
             case R.id.Room2:
-                intent.putExtra("Room",2);
-                Toast.makeText(this,"You have choosen the bathroom",Toast.LENGTH_SHORT).show();
+                //помещаем в intent значение 2
+                intent.putExtra("Room", 2);
+
+                //выводим сообщение на экран
+                Toast.makeText(this, "You have choosen the bathroom", Toast.LENGTH_SHORT).show();
+
+                //переходим на активность roomsActivity
                 startActivity(intent);
                 break;
 
+            //нажата кнопка "Bedroom"
             case R.id.Room3:
-                intent.putExtra("Room",3);
-                Toast.makeText(this,"You have choosen the bedroom",Toast.LENGTH_SHORT).show();
+                //помещаем в intent значение 3
+                intent.putExtra("Room", 3);
+
+                //выводим сообщение на экран
+                Toast.makeText(this, "You have choosen the bedroom", Toast.LENGTH_SHORT).show();
+
+                //переходим на активность roomsActivity
                 startActivity(intent);
                 break;
 
             default:
                 break;
         }
-
-
-
-
     }
 
-    private void GenerateButtons(int _number, LinearLayout.LayoutParams layParams){
+    /*********************************************************/
 
-       Button [] Buttons = new Button[_number];
+    /*
+    Метод для отрисовки кнопок на экране
+     */
 
-        for(int i = 0;i < Buttons.length;i++){
+    /*********************************************************/
 
-            Buttons[i] = new Button(this);
-            Buttons[i].setText("Room" + (1+i));
-            Buttons[i].setOnClickListener(this);
-            linearLayout.addView(Buttons[i],layParams);
-        }
+    private void GenerateRoomButton(Button [] b1, LinearLayout.LayoutParams layoutParams){
 
-    }
-
-    private void GenerateRoomButton(Button [] b1,LinearLayout.LayoutParams layoutParams){
-
+        //перебираем массив и каждый объект добавляем в linearLayout
         for (Button b: b1) {
             linearLayout.addView(b,layoutParams);
-
         }
     }
 
+    /*********************************************************/
+
+    /*
+    Методы для работы с меню
+     */
+
+    /*********************************************************/
+
+    //Создание menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
 
+        getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
     }
 
+    //Обработчик нажатия кнопок в menu
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
+        //получаем ID нажатого элемента в menu
         int id = item.getItemId();
 
         switch(id) {
 
+            //нажата кнопка "Quit"
             case R.id.quit:
-
+                //помещаем в intent1 значение 1
                 intent1.putExtra("Quit",1);
+
+                //переходим на активность MainActivity
                 startActivity(intent1);
                 return true;
-
 
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+    /*********************************************************/
 }
 
 
--- app/src/main/java/com/example/test/roomsActivity.java
+++ app/src/main/java/com/example/test/roomsActivity.java
@@ -5,193 +5,333 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
 public class roomsActivity extends AppCompatActivity implements View.OnClickListener {
 
+    /*
+    Переменные
+     */
 
+    /*********************************************************/
+
+    //переменная для хранения значения переданного из активности Main2Activity
     int room;
 
+    /*********************************************************/
+
+    /*
+    Объекты классов
+     */
+
+    /*********************************************************/
+
+    //параметры для кнопок
+    LinearLayout.LayoutParams layoutParams;
+
+    /*********************************************************/
+
+    /*
+    Элементы на экране
+     */
+
+    /*********************************************************/
+
+    //область на экране для размещения элементов
     LinearLayout linearLayout;
 
-    private LinearLayout.LayoutParams layoutParams;
+    /*********************************************************/
+
+    /*
+    Метод вызываемый при создании активности roomsActivity
+     */
+
+    /*********************************************************/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rooms);
 
-        linearLayout = (LinearLayout) findViewById(R.id.lineralMain);
-
+        //получения значения, передающего от активности Main2Activity
         room = getIntent().getExtras().getInt("Room");
 
-        layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        //нахождение элемента экрана по его ID
+        linearLayout = (LinearLayout) findViewById(R.id.lineralMain);
 
+        //инициализация объекта класса LinearLayout.LayoutParams
+        //WRAP_CONTENT - размер кнопок будет в зависимости от текста внутри
+        layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        //отступ сверху
         layoutParams.topMargin = 50;
 
+        //расположение по центру
         layoutParams.gravity = Gravity.CENTER;
 
-       switch (room){
-           case 1:
-               GenerateMainInformation(1,layoutParams,linearLayout);
-               break;
-           case 2:
-               GenerateMainInformation(2,layoutParams,linearLayout);
-               break;
-           case 3:
-               GenerateMainInformation(3,layoutParams,linearLayout);
-               break;
+        switch (room) {
+            //передано значение 1(выбрана комната "Kitchen")
+            case 1:
+                //выводим краткую информацию о действиях в кухне
+                GenerateMainInformation(1, layoutParams, linearLayout);
+                break;
 
-           default:
-               break;
-       }
+            //передано значение 2(выбрана комната "Bathroom")
+            case 2:
+                //выводим краткую информацию о дейстиях в ванной
+                GenerateMainInformation(2, layoutParams, linearLayout);
+                break;
 
+            //передано значение 3(выбрана комната "Bedroom")
+            case 3:
+                //выводим краткую информацию о действиях в спальне
+                GenerateMainInformation(3, layoutParams, linearLayout);
+                break;
 
+            default:
+                break;
+        }
     }
 
+    /*********************************************************/
+
+    /*
+    Обработчик нажатий кнопок
+     */
+
+    /*********************************************************/
 
     @Override
     public void onClick(View v) {
 
+        switch(v.getId()) {
 
-        switch(v.getId()){
-
+            //нажата кнопка "Show All Information"
             case R.id.AllInformation:
+                //убираем все элементы с экрана
                 RemoveAllView(linearLayout);
+
+                //отображаем все действия в комнате на экране
                 GenerateALlButton(room);
                 break;
 
+            //нажата кнопка "Door"
             case R.id.Door:
-
+                //находим элемент на экране по его ID
                 Switch Door = (Switch) findViewById(R.id.Door);
 
+                /*Если switch переключают в "активное" состояние, то
+                 *необходимо выдать сообщение об открытии двери,
+                 *в другом случае нужно сообщить о том что дверь была закрыта*/
+                if(Door.isChecked()) {
+                    //создаём сообщение
+                    Toast toast = Toast.makeText(this, "Door has opened", Toast.LENGTH_SHORT);
 
-                if(Door.isChecked())
-                {
-                   Toast toast = Toast.makeText(this,"Door has opened",Toast.LENGTH_SHORT);
-                    toast.setGravity(Gravity.CENTER,0,0);
+                    //ставим сообщению параметр, с помощью которого оно будет выводится по центру
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+
+                    //выводим сообщение на экран
                     toast.show();
-                }
-                else{
-                    Toast.makeText(this,"Door has closed",Toast.LENGTH_SHORT).show();
+                } else {
+                    //выводим сообщение на экран
+                    Toast.makeText(this, "Door has closed", Toast.LENGTH_SHORT).show();
                 }
                 break;
 
+            //нажата кнопка "Light"
             case R.id.Light:
-
+                //находим элемент на экране по его ID
                 Switch Light = (Switch) findViewById(R.id.Light);
 
-                if(Light.isChecked()){
-                    Toast toast = Toast.makeText(this,"Light has turned on",Toast.LENGTH_SHORT);
-                    toast.setGravity(Gravity.CENTER,0,0);
+                /*Если switch переключают в "активное" состояние,
+                 *то необходимо выдать сообщение о включении света,
+                 *в другом случае нужно сообщить о том что свет был выключен*/
+                if(Light.isChecked()) {
+                    //инициализируем объект сообщение
+                    Toast toast = Toast.makeText(this, "Light has turned on", Toast.LENGTH_SHORT);
+
+                    //ставим сообщению параметр, с помощью которого оно будет выводится по центру
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+
+                    //показываем сообщение
                     toast.show();
                 }
                 else {
-                    Toast.makeText(this,"Light has turned off",Toast.LENGTH_SHORT).show();
+                    //показываем сообщение на экране
+                    Toast.makeText(this, "Light has turned off", Toast.LENGTH_SHORT).show();
                 }
 
-
             default:
                 break;
-
         }
-
-
-
     }
 
+    /*********************************************************/
 
-    private void GenerateMainInformation(int roomNumber,LinearLayout.LayoutParams layoutParams,LinearLayout linearLayout)
-    {
-        switch (roomNumber){
+    /*
+    Метод для выведения на экран краткой информации по комнате и кнопки "Show All Information"
+     */
+
+    /*********************************************************/
+
+    private void GenerateMainInformation(int roomNumber,
+                                         LinearLayout.LayoutParams layoutParams,
+                                         LinearLayout linearLayout) {
+        switch (roomNumber) {
+            //выбрана комната "Kitchen"
             case 1:
-                GenerateDoorButton(linearLayout,layoutParams);
-                GenerateAllInformationButton(linearLayout,layoutParams);
+                //выводит на экран кнопку двери
+                GenerateDoorButton(linearLayout, layoutParams);
+
+                //выводит на экран кнопку показа всей информаци
+                GenerateAllInformationButton(linearLayout, layoutParams);
                 break;
 
+            //выбрана комната "Bathroom"
             case 2:
-                GenerateDoorButton(linearLayout,layoutParams);
-                GenerateAllInformationButton(linearLayout,layoutParams);
+                //выводит на экран кнопку двери
+                GenerateDoorButton(linearLayout, layoutParams);
+
+                //выводит на экран кнопку показа всей информаци
+                GenerateAllInformationButton(linearLayout, layoutParams);
                 break;
 
+            //выбрана комната "Bedroom"
             case 3:
-                GenerateDoorButton(linearLayout,layoutParams);
-                GenerateAllInformationButton(linearLayout,layoutParams);
+                //выводит на экран кнпоку двери
+                GenerateDoorButton(linearLayout, layoutParams);
+
+                //выводит на экран кнопку показа всей информаци
+                GenerateAllInformationButton(linearLayout, layoutParams);
                 break;
 
             default:
                 break;
         }
-
-
-
     }
 
-    private void GenerateDoorButton(LinearLayout linearLayout,LinearLayout.LayoutParams layoutParams)
-    {
+    /*********************************************************/
 
+    /*
+    Методы для создания и отрисовки элементов на экране
+     */
 
-       Switch Door = new Switch(this);
+    /*********************************************************/
 
+    //создание "кнопки" двери
+    private void GenerateDoorButton(LinearLayout linearLayout,
+                                    LinearLayout.LayoutParams layoutParams) {
+        //инициализация объекта Switch
+        Switch Door = new Switch(this);
+
+        //установка текста
         Door.setText("Door");
+
+        //установка ID
         Door.setId(R.id.Door);
+
+        //установка обработчика нажатий на Switch двери
         Door.setOnClickListener(this);
 
-        linearLayout.addView(Door,layoutParams);
+        //добавление "кнопки" двери в linearLayout
+        linearLayout.addView(Door, layoutParams);
     }
 
-    private void GenerateLightButton(LinearLayout linearLayout,LinearLayout.LayoutParams layoutParams)
-    {
+    //создание "кнопки" света
+    private void GenerateLightButton(LinearLayout linearLayout,
+                                     LinearLayout.LayoutParams layoutParams) {
+        //инициализация объекта Switch
         Switch Light = new Switch(this);
 
+        //установка текста
         Light.setText("Light");
+
+        //установка ID
         Light.setId(R.id.Light);
+
+        //установка обработчика нажатий на Switch света
         Light.setOnClickListener(this);
 
-        linearLayout.addView(Light,layoutParams);
+        //добавление "кнопки" света в linearLayout
+        linearLayout.addView(Light, layoutParams);
     }
 
-    private void GenerateAllInformationButton(LinearLayout linearLayout,LinearLayout.LayoutParams layoutParams){
-
+    //создание вспомогательной кнопки всей информации о комнате
+    private void GenerateAllInformationButton(LinearLayout linearLayout,
+                                              LinearLayout.LayoutParams layoutParams) {
+        //инициализация кнопки
         Button AllInformation = new Button(this);
 
+        //установка текста
         AllInformation.setText("Show All Information");
+
+        //установка ID
         AllInformation.setId(R.id.AllInformation);
+
+        //установка обработчика нажатий на кнопку
         AllInformation.setOnClickListener(this);
 
-        linearLayout.addView(AllInformation,layoutParams);
+        //добавление кнопки общей информации в linearLayout
+        linearLayout.addView(AllInformation, layoutParams);
     }
+    /*********************************************************/
 
-    private void RemoveAllView(LinearLayout linearLayout){
+    /*
+    Метод удаления всех элементов с экрана
+     */
+
+    /*********************************************************/
+
+    private void RemoveAllView(LinearLayout linearLayout) {
+        //удаление из главного linearLayout всех элементов
         linearLayout.removeAllViewsInLayout();
     }
 
-    private void GenerateALlButton(int roomNumber){
+    /*********************************************************/
 
-        switch (roomNumber){
+    /*
+    Метод выведения на экран всей информации о комнате
+     */
+
+    /*********************************************************/
+
+    private void GenerateALlButton(int roomNumber) {
+
+        switch (roomNumber) {
+            //выбрана комната "Kitchen"
             case 1:
-                GenerateDoorButton(linearLayout,layoutParams);
-                GenerateLightButton(linearLayout,layoutParams);
+                //вывести на экран кнопку двери
+                GenerateDoorButton(linearLayout, layoutParams);
+
+                //вывести на экран кнпоку света
+                GenerateLightButton(linearLayout, layoutParams);
                 break;
 
+            //выбрана комната "Bathroom"
             case 2:
-                GenerateDoorButton(linearLayout,layoutParams);
-                GenerateLightButton(linearLayout,layoutParams);
+                //вывести на экран кнпоку двери
+                GenerateDoorButton(linearLayout, layoutParams);
+
+                //вывести на экран кнопку света
+                GenerateLightButton(linearLayout, layoutParams);
                 break;
 
+            //выбрана комната "Bedroom"
             case 3:
-                GenerateDoorButton(linearLayout,layoutParams);
-                GenerateLightButton(linearLayout,layoutParams);
+                //вывести на экран кнопку двери
+                GenerateDoorButton(linearLayout, layoutParams);
+
+                //вывести на экран кнопку света
+                GenerateLightButton(linearLayout, layoutParams);
                 break;
 
             default:
                 break;
         }
-
     }
 
-
+    /*********************************************************/
 }
